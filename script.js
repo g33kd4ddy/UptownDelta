45cefbdcf866b6434a8cab80d12b1a7885eefea7
g33kd4ddy <bbuchan0@gmail.com>
2026-01-04 00:13:05 -0500

    Add Social links and fade in main photo.

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ document.querySelectorAll(".content-section").forEach(s=>s.classList.remove("vis
 document.getElementById(id).classList.add("visible");
 window.scrollTo({top:0,behavior:"smooth"});
 }
+const image = document.getElementById("responsiveImage");
+    // Trigger fade-in once the image is fully loaded
+    if (image.complete) {
+        image.classList.add("fade-in");
+    } else {
+        image.addEventListener("load", () => {
+            image.classList.add("fade-in");
+        });
+    }
